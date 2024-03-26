e54753a7430d68cf5ceea65b6b69e1f9b132b867
Rabby <67389402+RabbyDevs@users.noreply.github.com>
2024-03-26 05:36:48 +0300

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require('express');
 const app = express ();
 app.use(express.json());
-const PORT = process.env.PORT || 2530;
+const PORT = process.env.PORT || 2540;
 if (!fs.existsSync('./cache')) fs.mkdirSync('./cache')
 const axios = require('axios');
 
