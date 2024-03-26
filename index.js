99c93385ecc48e09472ca4101611613d7468d4b9
Rabby <67389402+RabbyDevs@users.noreply.github.com>
2024-03-26 05:31:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const express = require('express');
+const app = express ();
+app.use(express.json());
+const PORT = process.env.PORT || 2530;
+if (!fs.existsSync('./cache')) fs.mkdirSync('./cache')
+const axios = require('axios');
+
+app.post('/webhook', async(request, response) => {
+    console.log(request.body)
+    console.log(request.query)
+})
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
