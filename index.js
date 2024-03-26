dc793cc0a5b68f19aecb1a0de577390c3b9d9d87
Rabby <67389402+RabbyDevs@users.noreply.github.com>
2024-03-26 05:50:50 +0300

    w

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const PORT = process.env.PORT || 2540;
 if (!fs.existsSync('./cache')) fs.mkdirSync('./cache')
 const axios = require('axios');
 
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+
 app.post('/webhook', async(request, response) => {
     console.log(request.body)
-    console.log(request.query)
+    const id = request.query.id
+    const token = request.query.auth
+    const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
+    const decryptedBody = JSON.parse(request.body)
+    webhookClient.send({
+        content: decryptedBody.content,
+        embeds: decryptedBody.embeds
+    })
 })
 
 app.listen(PORT, () => {
